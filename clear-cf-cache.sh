6a1c6d5e32d2990301774b81d986e8b5cce7ec9b
Michael Leer <me@trozzy.net>
2016-07-11 11:12:56 +0100

    add script to clear cache due to broken if statement in CodeShip

---
+++ clear-cf-cache.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ $(grep cf_full_purge config.toml) =~ true$ ]]; then
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$DOMAIN/purge_cache" \
+-H "X-Auth-Email: $EMAIL" \
+-H "X-Auth-Key: $KEY" \
+-H "Content-Type: application/json" \
+--data '{"purge_everything":true}'
+else
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$DOMAIN/purge_cache" \
+-H "X-Auth-Email: $EMAIL" \
+-H "X-Auth-Key: $KEY" \
+-H "Content-Type: application/json" \
+--data '{"files":["https://www.trozzy.net/", "https://www.trozzy.net/post/"]}';
+fi
